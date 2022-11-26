bcc84dde830ac720ce82af64034423ec8c2231a6
Lixie Labs <connornishijima@gmail.com>
2022-11-25 20:06:59 -0700

    Update Motion_Detection_Demo.ino

--- examples/Motion_Detection_Demo/Motion_Detection_Demo.ino
+++ examples/Motion_Detection_Demo/Motion_Detection_Demo.ino
@@ -5,13 +5,11 @@
 // Top graph is motion, bottom graph is proximity!
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(250000);
   motion_start(A0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   Serial.print("-300\t300\t0\t");
   Serial.print(motion()+150);
   Serial.print("\t");
